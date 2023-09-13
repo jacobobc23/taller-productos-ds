62dfa5cc98faaca79f6d485d0265f598e1a52551
Johan0425 <114786001+Johan0425@users.noreply.github.com>
2023-09-13 10:38:58 -0500

    modificacion clases dao a minus

--- src/view/main/CategoryView.java
+++ src/view/main/CategoryView.java
@@ -334,7 +334,6 @@ public class CategoryView extends javax.swing.JFrame {
                     product.getCategory().getCategoryName(),
                     product.getPrice()
                 });
-
             }
 
         } else {
